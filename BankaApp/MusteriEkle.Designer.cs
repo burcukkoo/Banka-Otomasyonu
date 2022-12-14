cbf9403814a9bdc830587feda3c3a62471000d0d
Burcu Öztürk <burcu.ozturk@bilgeadamboost.com>
2022-12-15 01:56:29 +0300

    Add project files.

---
+++ BankaApp/MusteriEkle.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace BankaApp
+{
+    partial class MusteriEkle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lblBankaAdi = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBakiye = new System.Windows.Forms.TextBox();
+            this.txtTcNo = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(415, 426);
+            this.btnEkle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(339, 90);
+            this.btnEkle.TabIndex = 23;
+            this.btnEkle.Text = "müşteri ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lblBankaAdi
+            // 
+            this.lblBankaAdi.AutoSize = true;
+            this.lblBankaAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBankaAdi.Location = new System.Drawing.Point(391, 96);
+            this.lblBankaAdi.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblBankaAdi.Name = "lblBankaAdi";
+            this.lblBankaAdi.Size = new System.Drawing.Size(0, 46);
+            this.lblBankaAdi.TabIndex = 22;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(375, 11);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(372, 46);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "MÜŞTERİ EKLEME";
+            // 
+            // txtBakiye
+            // 
+            this.txtBakiye.Location = new System.Drawing.Point(360, 372);
+            this.txtBakiye.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtBakiye.Name = "txtBakiye";
+            this.txtBakiye.Size = new System.Drawing.Size(457, 22);
+            this.txtBakiye.TabIndex = 20;
+            // 
+            // txtTcNo
+            // 
+            this.txtTcNo.Location = new System.Drawing.Point(360, 327);
+            this.txtTcNo.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtTcNo.Name = "txtTcNo";
+            this.txtTcNo.Size = new System.Drawing.Size(457, 22);
+            this.txtTcNo.TabIndex = 19;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(360, 283);
+            this.txtSoyad.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(457, 22);
+            this.txtSoyad.TabIndex = 18;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(360, 239);
+            this.txtAd.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(457, 22);
+            this.txtAd.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(248, 379);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 31);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "bakiye";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(252, 335);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 31);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "tc no:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(251, 290);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 31);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "soyad";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(279, 246);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 31);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "ad";
+            // 
+            // MusteriEkle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.lblBankaAdi);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtBakiye);
+            this.Controls.Add(this.txtTcNo);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MusteriEkle";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.MusteriEkle_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Label lblBankaAdi;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtBakiye;
+        private System.Windows.Forms.TextBox txtTcNo;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
